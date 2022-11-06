2a2f2b2894981abcb393df8401c64caa83f525da
SNirloy <yolrinidas@gmail.com>
2022-11-06 18:46:20 -0500

    I believe I completed the assignment. We'll see though

--- 19_session/app.py
+++ 19_session/app.py
@@ -1,5 +1,8 @@
 """
-
+Gastric Bypass Train: Sam, Anna, Sadi
+K19-Sessions Greetings
+Estimated Time: 2 hours
+6 November, 2022
 """
 from flask import Flask
 from flask import session
@@ -13,39 +16,37 @@ app.secret_key = os.urandom(32)
 
 
 logins = {
-    "gastric bypass train": "Heebiejeebies"
+    "tophr": "mykolyk"
 }
-"""
-@app.route("/", methods = ["POST", "GET"])
-def login_page():
-    print(len(request.form))
-    print(len(request.args))
-    if(len(request.form) == 0 and len(request.args) == 0):
-      return render_template("login.html")
-    print(request.form)
-    print(request.args)
-    return render_template("login.html")
-"""
 
 @app.route("/", methods = ["POST", "GET"])
 def home_page():
+  #Some print lines
     print(len(request.form))
     print(len(request.args))
     print(session)
     print(request.form)
     print(request.args)
+  #If a post form is received, set up the session
     if(len(request.form) > 0):
         session["username"] = request.form["username"]
         session["password"] = request.form["password"]
+  #If a get form is received, set the session
     if(len(request.args) > 0):
         session["username"] = request.args["username"]
         session["password"] = request.args["password"]
+  #If there is a session currently
     if(len(session) > 0):
+    
+    #If the username is not registered in login
       if(not session["username"] in logins.keys()):
         return "<h1>Who are you? I have no recollection of a " + str(session["username"]) + ".</h1>"
+    #If the password does not match the provided username
       if (not logins[session["username"]] == session["password"]):
         return "<h1> Not the right password. SOUND THE INTRUDER ALERT!!!</h1>"
+    #If the username is in logins and the password in session matches
       return render_template("response.html", username = session["username"])
+  #If there is no session
     return render_template("login.html")
 
 @app.route("/logout")
