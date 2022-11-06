44a6d8a455ad99a980278fc7f6b81a13d1914180
SNirloy <yolrinidas@gmail.com>
2022-11-06 17:34:13 -0500

    Some stuff is working. Committing for when I inevitably ruin everything

--- 19_session/app.py
+++ 19_session/app.py
@@ -5,10 +5,12 @@ from flask import Flask
 from flask import session
 from flask import render_template
 from flask import request
+from flask import redirect, url_for
 import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
+
 
 logins = {
     "gastric bypass train": "Heebiejeebies"
@@ -16,18 +18,30 @@ logins = {
 
 @app.route("/")
 def login_page():
+    print(request.form)
     return render_template("login.html")
 
 @app.route("/response", methods = ["POST", "GET"])
 def response():
-    for i in request.form:
-      session[i] = request.form[i]
-    #print(session);
-    #for i in session: 
-    #  print(str(i) + ": " + str(session[i]))
-    if (session["username"] in logins.keys() and logins[session["username"]] == session["password"]):
-    	return render_template("response.html")
-    return render_template("login.html") 
+    if(request.method == "POST"):
+        session["username"] = request.form["username"]
+        session["password"] = request.form["password"]
+    elif(request.method == "GET"):
+        session["username"] = request.args["username"]
+        session["password"] = request.args["password"]
+
+    if(not session["username"] in logins.keys()):
+      return "<h1>Who are you? I have no recollection of a " + str(session["username"]) + ".</h1>"
+    if (not logins[session["username"]] == session["password"]):
+      return "<h1> Not the right password. SOUND THE INTRUDER ALERT!!!</h1>"
+    return render_template("response.html", username = session["username"])
+
+@app.route("/logout")
+def logout():
+  session.pop("username")
+  session.pop("password")
+  print(session)
+  return render_template("login.html")
 
 app.Debug = True
 app.run()
