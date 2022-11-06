7c3071ee9baaecca14460a765ecdc8ea4d35c2df
SNirloy <yolrinidas@gmail.com>
2022-11-06 18:11:51 -0500

    Everything is functional, need some design and html edits before total completion

--- 19_session/app.py
+++ 19_session/app.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask import session
 from flask import render_template
 from flask import request
-from flask import redirect, url_for
+from flask import redirect
 import os
 
 app = Flask(__name__)
@@ -15,33 +15,46 @@ app.secret_key = os.urandom(32)
 logins = {
     "gastric bypass train": "Heebiejeebies"
 }
-
-@app.route("/")
+"""
+@app.route("/", methods = ["POST", "GET"])
 def login_page():
+    print(len(request.form))
+    print(len(request.args))
+    if(len(request.form) == 0 and len(request.args) == 0):
+      return render_template("login.html")
     print(request.form)
+    print(request.args)
     return render_template("login.html")
+"""
 
-@app.route("/response", methods = ["POST", "GET"])
-def response():
-    if(request.method == "POST"):
+@app.route("/", methods = ["POST", "GET"])
+def home_page():
+    print(len(request.form))
+    print(len(request.args))
+    print(session)
+    print(request.form)
+    print(request.args)
+    if(len(request.form) > 0):
         session["username"] = request.form["username"]
         session["password"] = request.form["password"]
-    elif(request.method == "GET"):
+    if(len(request.args) > 0):
         session["username"] = request.args["username"]
         session["password"] = request.args["password"]
-
-    if(not session["username"] in logins.keys()):
-      return "<h1>Who are you? I have no recollection of a " + str(session["username"]) + ".</h1>"
-    if (not logins[session["username"]] == session["password"]):
-      return "<h1> Not the right password. SOUND THE INTRUDER ALERT!!!</h1>"
-    return render_template("response.html", username = session["username"])
+    if(len(session) > 0):
+      if(not session["username"] in logins.keys()):
+        return "<h1>Who are you? I have no recollection of a " + str(session["username"]) + ".</h1>"
+      if (not logins[session["username"]] == session["password"]):
+        return "<h1> Not the right password. SOUND THE INTRUDER ALERT!!!</h1>"
+      return render_template("response.html", username = session["username"])
+    return render_template("login.html")
 
 @app.route("/logout")
 def logout():
   session.pop("username")
   session.pop("password")
   print(session)
-  return render_template("login.html")
+  return redirect("/")
 
-app.Debug = True
-app.run()
+if __name__ == "__main__":
+  app.debug = True
+  app.run()
