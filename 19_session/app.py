e5266e4fcfbe65b480f0da77cf6ce480411928ea
SNirloy <yolrinidas@gmail.com>
2022-11-06 01:15:11 -0400

    Start of a working login system

--- 19_session/app.py
+++ 19_session/app.py
@@ -5,9 +5,10 @@ from flask import Flask
 from flask import session
 from flask import render_template
 from flask import request
+import os
 
 app = Flask(__name__)
-app.secret_key = b"help"
+app.secret_key = os.urandom(32)
 
 logins = {
     "gastric bypass train": "Heebiejeebies"
@@ -19,13 +20,14 @@ def login_page():
 
 @app.route("/response", methods = ["POST", "GET"])
 def response():
-    session["username"] = request.form["username"]
-    session["password"] = request.form["password"]
-    
-    username = request.cookies.get(session["username"])
-    print(username)
-    print(request.cookies)
-    return render_template("response.html")
+    for i in request.form:
+      session[i] = request.form[i]
+    #print(session);
+    #for i in session: 
+    #  print(str(i) + ": " + str(session[i]))
+    if (session["username"] in logins.keys() and logins[session["username"]] == session["password"]):
+    	return render_template("response.html")
+    return render_template("login.html") 
 
 app.Debug = True
 app.run()
