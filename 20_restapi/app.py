91897adbcb4c2188c8f88c5d5c8708a14651e35a
Sadi Nirloy <snirloy30@stuy.edu>
2022-11-22 13:14:12 -0500

    K20 completed I guess

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,13 +1,25 @@
+"""
+Table Corner: Thomas Zhang, Sadi Nirloy
+Softdev
+K20 -- A RESTful Journey Skyward: An introduction to Rest APIs
+2022-11-22
+time spent: 1.0 hr
+"""
 import requests
 from flask import render_template, Flask
 
 app = Flask(__name__)
+api_key = ""
+with open("key_nasa.txt") as api:
+	api_key = api.readline().replace("\n", "")
 
 @app.route("/")
 def home_page():
-	response = requests.get("https://api.nasa.gov/planetary/apod?api_key=H3S4NkgQuewjgC8owP3DZIVgPmFdpgjhP0fSYgCa")
+	link = "https://api.nasa.gov/planetary/apod?api_key=" + api_key
+	print("Key: " + link)
+	response = requests.get(link)
 	dictionary = response.json()
-	return dictionary
+	return render_template("main.html", title = dictionary["title"], image = dictionary["url"], description = dictionary["explanation"])
 
 if __name__ == "__main__":
 	app.debug = True
