34f2e5c0b902b04fe88ba59f20e2d4d295c8db78
SNirloy <yolrinidas@gmail.com>
2023-04-24 23:30:45 -0400

    Commented up

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -17,25 +17,25 @@ var toggleMode = (e) => {
         mode = "rect";
     }
     var bToggler = document.getElementById("buttonToggle");
-    bToggler.innerHTML = mode;
+    bToggler.innerHTML = mode; // Changing the HTML inside the button in order to change the word in it
 }
 
 var drawRect = function(e) {
     var mouseX = e.offsetX; //gets X-coor of mouse when event is fired
     var mouseY = e.offsetY; //gets Y-coor of mouse when event is fired
     console.log("mouseclick registered at ", mouseX, mouseY);
-    ctx.fillRect(mouseX, mouseY, 10, 10);
+    ctx.fillRect(mouseX, mouseY, 20, 20);
 }
 
 var drawCircle = function(e) {
     var mouseX = e.offsetX;
     var mouseY = e.offsetY;
     console.log("mouseclick registered at ", mouseX, mouseY);
-    ctx.beginPath();
+    ctx.beginPath(); // A path is needed in order to isolate steps in coloring
     ctx.fillStyle = "black"; //circle color
-    ctx.strokeStyle = "black";
+    ctx.strokeStyle = "black"; // outline color
     ctx.arc(mouseX, mouseY, 10, 0, 2*Math.PI);
-    ctx.fill();
+    ctx.fill(); // This fill will color in the spaces between shapes unless it is put inside of a path.
     ctx.closePath();
 }
 
@@ -51,7 +51,7 @@ var draw = (e) => {
 
 //var wipeCanvas - function(){}
 var wipeCanvas = () => {
-    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.clearRect(0, 0, c.width, c.height); // Changes the colors of all pixels. THIS IS DIFFERENT FROM THROWING ON ANOTHER WHITE RECTANGLE ON TOP
 }
 
 c.addEventListener("click", draw) //passes the mouse event as parameter for the function
