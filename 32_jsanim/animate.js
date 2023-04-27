44bb9769310125cc5bf67332dda6e27477e31372
SNirloy <yolrinidas@gmail.com>
2023-04-26 21:19:25 -0400

    Thing works me thinks

--- 32_jsanim/animate.js
+++ 32_jsanim/animate.js
@@ -1,18 +1,17 @@
 /*
-James Yu, Sadi Nirloy 
+Bread Wang, Sadi Nirloy 
 */
 
 var c = document.getElementById("playground");
 var dotButton = document.getElementById("buttonCircle");
+var saveButton = document.getElementById("buttonRect");
 var stopButton = document.getElementById("buttonStop");
-var screenButton = document.getElementById("dvdButton");
 
 var ctx = c.getContext("2d");
 
 var requestID;
 
 var clear = (e) =>{
-    e.preventDefault(); // To prevent some kind of unnecessary repetition
     ctx.clearRect(0, 0, c.width, c.height);
 }
 
@@ -21,7 +20,7 @@ var growing = true;
 var smaller = Math.min(c.width, c.height);
 
 var drawDot = () => {
-    // window.cancelAnimationFrame(requestID); // to prevent the acceleration of animation
+    window.cancelAnimationFrame(requestID); // to prevent the acceleration of animation
     clear();
     ctx.beginPath(); // A path is needed in order to isolate steps in coloring
     ctx.fillStyle = "black"; //circle color
@@ -43,46 +42,45 @@ var drawDot = () => {
         growing = true;
     }
     requestID = window.requestAnimationFrame(drawDot);
-    dotButton.removeEventListener("click", drawDot); // I used this method to take away the power of the button to add layers of animation.
-    // I need to do this since this method needs to requestAnimationFrame itself at all times it is running, so I could not limit its access to that step.
 }
 
 var stopIt = () =>{
     window.cancelAnimationFrame(requestID);
-    dotButton.addEventListener("click", drawDot);
 }
 
-var dvdLogoSetup = function(){
+var drawSave = () =>{
+    console.log("Gottem");
     window.cancelAnimationFrame( requestID); // Uses the same requestID in order to have only "one animation" play at a time
+    
+    var logo = new Image();
+    logo.src = "logo_dvd.jpg";
+
     var rectWidth = 100;
     var rectHeight = 50;
 
-    var rectX = Math.floor(Math.random() * (c.height - rectHeight));
-    var rectY = Math.floor(Math.random() * (c.width - rectWidth));
+    var rectX = Math.floor(Math.random() * (c.width - rectWidth));
+    var rectY = Math.floor(Math.random() * (c.height - rectHeight));
 
-    var xVel = Math.floor(Math.random(30));
-    var yVel = Math.floor(Math.random(30));
-
-    var logo = new Image();
-    logo.src = "logo_dvd.jpg";
+    var xVel = Math.floor(Math.random(9)) + 1;
+    var yVel = Math.floor(Math.random(9)) + 1;
 
     var dvdLogo = function() {
         clear();
         ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
-        if (rectX <= 0 || rectX + rectHeight >= c.height){
+        console.log(rectX + ", " + rectY);
+        if (rectX <= 0 || rectX + rectWidth >= c.width){
             xVel *= -1;
         }
-        if (rectY <= 0 || rectY + rectWidth >= c.width){
+        if (rectY <= 0 || rectY + rectHeight >= c.height){
             yVel *= -1;
         }
         rectX += xVel;
         rectY += yVel;
-        requestID = window.requestAnimationFrame(dvdLogoSetup);
+        requestID = window.requestAnimationFrame(dvdLogo);
     };
     dvdLogo();
-
-}
+};
 
 dotButton.addEventListener("click", drawDot);
+saveButton.addEventListener("click", drawSave);
 stopButton.addEventListener("click", stopIt);
-dvdButton.addEventListener("click", dvdLogoSetup);
